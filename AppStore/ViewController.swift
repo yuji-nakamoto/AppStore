847117fb51d1928a280c858ab3a38701bfb0708e
yuji-nakamoto <yuji0411@icloud.com>
2020-06-25 18:22:37 +0900

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by yuji_nakamoto on 2020/06/25.
+//  Copyright © 2020 yuji_nakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
